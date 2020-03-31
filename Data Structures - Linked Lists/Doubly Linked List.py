85629ee5e3f038d7b48a884b789bc3cca6bc65fb
Vanishree Nayak <31129366+sunshine399@users.noreply.github.com>
2020-03-31 11:08:19 +0530

    line 49
    remove node at index 0

--- Data Structures - Linked Lists/Doubly Linked List.py
+++ Data Structures - Linked Lists/Doubly Linked List.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:355a0dd20fba67bd845be94eec9417e611776127d16864c19c25780e335b08e7
-size 2147
+oid sha256:4399d6c9ce4a5660f0403fd345ac4fbc6873bd07cc64c478b1794321349a45e5
+size 2251
