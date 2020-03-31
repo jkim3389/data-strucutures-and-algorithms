229a2cc376b309314cbe75e73e1bb6601f7ab8e9
Vanishree Nayak <31129366+sunshine399@users.noreply.github.com>
2020-03-31 09:03:04 +0530

    line  41
    if index 0 is passed

--- Data Structures - Linked Lists/Linked List.py
+++ Data Structures - Linked Lists/Linked List.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ef22c53b1ac60a47e02c7aeca98d661950943bfa84e98befb9dd8dd84a4a022
-size 1876
+oid sha256:406f58da1ab0e9976fc8885a39635b927bd9b48435412f2ce01716f5904107c0
+size 1932
