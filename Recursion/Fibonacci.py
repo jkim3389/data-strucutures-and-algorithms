4f91145026bc409ee44dcfd76cda6990cb178eef
Vitor Alves Raquel <74428540+Vitoralvesraquel@users.noreply.github.com>
2021-04-11 14:21:22 -0300

    Update Fibonacci.py and close issue #23.
    I was verifying issue #23. I generated two lists to compare the results with the fibonacci sequence and noted a problem:
    
    fib(num) returns:           [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]
    fibonacci(num) returns: [0, 1, 1, 3, 5, 8, 13, 21, 34, 55]
    fibonacci sequence:      [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
    
    I made some changes that can solve the problem. I also changed the variable "sum" to "total", because Python already has a sum() function.

--- Recursion/Fibonacci.py
+++ Recursion/Fibonacci.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b8e7cbb5a16d323846f0af74b7ed780c99fadc0cfc03e4670d56fc06fdc008e
-size 232
+oid sha256:3ab70a2b0eb7b026eae1b2cf14e9c61992707c18d1756c6c25c7eda94da4502b
+size 394
